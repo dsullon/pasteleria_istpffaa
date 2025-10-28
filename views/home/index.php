01058891350b389d63e9ac5fe0b80d69a967132c
Alex Sullon <alexsullonporras@hotmail.com>
2025-10-28 18:47:25 -0500

    Se implementó la lectura de las categorías en el home de la página

--- views/home/index.php
+++ views/home/index.php
@@ -1,11 +1,14 @@
 <div class="categorias contenedor">
     <div class="categorias__contenido">
         <?php foreach($categorias as $categoria): ?>
-            <div class="servicio">
+            <div class="categoria">
                 <img src="/assets/img/categorias/<?php echo $categoria->imagen ?>" 
                 alt="<?php echo $categoria->nombre; ?>">
                 <h3><?php echo $categoria->nombre; ?></h3>
-                <p><?php echo $categoria->descripcion; ?></p>
+                <div class="overlay">
+                    <p><?php echo $categoria->descripcion; ?></p>
+                    <a href="#">Ver más</a>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
